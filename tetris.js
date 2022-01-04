abf44ce453667b8f8cab3636ced7340b3d394eea
Mehmet Ali ÇAKIR <mehmetalicakir.int@gmail.com>
2022-01-04 15:23:48 +0300

    added board and pieces

--- tetris.js
+++ tetris.js
@@ -8,10 +8,78 @@ const VACANT = "WHITE"; // Color of an empty square
 
 //Draw a square
 
-function draw() {
-ctx.fillStyle = "red";
-ctx.fillRect(0,0,50,50);
+function drawSquare(x,y,color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SQ,y*SQ,SQ,SQ);
 
-ctx.strokeStyle = "BLACK";
-ctx.strokeReck(0,0,50,50);
+    ctx.strokeStyle = "BLACK";
+    ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
 }
+
+// Create the board
+
+let board = [];
+for (let r = 0; r < ROW; r++) {
+    board[r] = [];
+    for (let c = 0; c < COL; c++) {
+        board[r][c] = VACANT;
+        
+    }
+    
+}
+
+// Draw the board
+function drawBoard() {
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
+            drawSquare(c,r,board[r][c]);
+        }
+        
+    }
+    
+}
+drawBoard();
+
+const PIECES = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"yellow"],
+    [O,"blue"],
+    [L,"Purple"],
+    [I,"cyan"],
+    [J,"orange"]
+];
+
+// Initiate a piece
+
+let p = new Piece(PIECES[0][0],PIECES[0][1]);
+
+
+// The Object Piece
+
+function Piece(tetromino,color) {
+    this.tetromino = tetromino;
+    this.color = color;
+
+    this.tetrominoN = 0;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+
+    // Control the pieces
+    this.x = 0;
+    this.y = 0;
+}
+
+// Draw a piece to the board
+
+Piece.prototype.draw = function() {
+    for (let r = 0; r < this.activeTetromino.length; r++) {
+        for (let c = 0; c < this.activeTetromino.length; c++) {
+            // Draw only occupied squares
+            if(this.activeTetromino[r][c]) {
+                drawSquare(this.x+c,this.y+r,this.color);
+            }
+        }
+        
+    }
+}
+p.draw();
